e220ada7404277b45956b0aeb2b02eef177321ed
ahmad-sarfraz345 <muhammad.ahmad.sarfraz17573@gmail.com>
2026-02-16 13:34:13 +0500

    Added Circle class

---
+++ app/src/main/java/com/example/device/Circle.java
@@ -0,0 +1,16 @@
+package com.example.device;
+
+public class Circle extends Shape {
+
+    private int radius;
+
+    public Circle(int x, int y, int radius) {
+        super(x, y);
+        this.radius = radius;
+    }
+
+    @Override
+    public double area() {
+        return Math.PI * radius * radius;
+    }
+}
